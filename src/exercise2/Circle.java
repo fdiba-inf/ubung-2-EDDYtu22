adf21f3b322a500007c623f441d44c76c748d0b4
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-10-14 09:23:33 +0300

    Ubung 2

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Circle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Radius:");
-        int radius = input.nextInt();
+        //System.out.print("Radius:");
+        double radius = input.nextDouble();
         double Pi = 4.0*(1.0-1.0/3.0+1.0/5.0-1.0/7.0+1.0/11.0-1/13.0+1.0/17.0-1.0/19.0+1.0/23.0);
         double Circumface = 2.0*Pi*radius;
         double area = Pi*radius*radius;
-        System.out.println("Circumface:" + Circumface);
-        System.out.println("Area:" + area);
+        System.out.println("Circumface: " + Circumface);
+        //System.out.println("Area:" + area);
 
 
     }
--- src/exercise2/PiApproximation.java
+++ src/exercise2/PiApproximation.java
@@ -3,11 +3,11 @@ import java.lang.Math;
 
 public class PiApproximation {
     public static void main(String[] args) {
-        float Pi = (float) (4.0*(1.0-1.0/3.0+1.0/5.0-1.0/7.0+1.0/11.0-1/13.0+1.0/17.0-1.0/19.0+1.0/23.0));
-        int a = (int) (Pi*10000);
-        double b = (double) a/10000;
+        double Pi = (4.0*(1.0-1.0/3.0+1.0/5.0-1.0/7.0+1.0/11.0-1/13.0+1.0/17.0-1.0/19.0+1.0/23.0));
+        //int a = (int) (Pi*10000);
+        //double b = (double) a/10000;
 
-        System.out.println("Pi: " + b);
+        System.out.println("Pi: " + Pi);
 
     }
 }
