e523c3e05e513b8bf11fe0216c11c5bb314a525a
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-10-14 09:32:02 +0300

    Ubung 2

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -1,16 +1,17 @@
 package exercise2;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Circle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //System.out.print("Radius:");
         double radius = input.nextDouble();
-        double Pi = 3.14;
-        double Circumface = 2.0*Pi*radius;
-        double area = Pi*radius*radius;
-        System.out.println("Circumference: " + Circumface);
+        double Pi = Math.PI;
+        double Circumference = 2.0*Pi*radius;
+        //double area = Pi*radius*radius;
+        System.out.println("Circumference: " + Circumference);
         //System.out.println("Area:" + area);
 
 
